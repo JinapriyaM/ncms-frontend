e4af2da3ee63d780fced66f6ba15de36f3c4aa72
JinapriyaM <madushankakulasinghe@gmail.com>
2020-10-23 08:20:45 +0530

    change login

---
+++ src/containers/Signinup/SignIn.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const SignIn = () => {
+    return (
+        <div>
+            signin page
+        </div>
+    )
+}
+
+export default SignIn;
---
+++ src/containers/Signinup/in.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const login = () => {
+  return (
+    <div>
+      <h1>hello i
+          n the world
+          </h1>
+    </div>
+  );
+};
+
+export default login;
